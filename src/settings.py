1eef1ecaf0c2a84d674af9554f75e7262a6a26c1
peixed351 <peixed351@gmail.com>
2024-02-23 17:42:08 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Environmental Microorganism Image Dataset"
 PROJECT_NAME_FULL: str = "EMDS-6: Environmental Microorganism Image Dataset Sixth Version"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
